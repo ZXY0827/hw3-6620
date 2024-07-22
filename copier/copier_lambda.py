9b3f0969b0212fe7a87e29316eea8f772cdff0f5
ZXY0827 <97982622+ZXY0827@users.noreply.github.com>
2024-07-22 14:24:39 -0700

    Update copier_lambda.py

--- copier/copier_lambda.py
+++ copier/copier_lambda.py
@@ -33,42 +33,13 @@ def lambda_handler(event, context):
                 for obj in page['Contents']:
                     if "temp" in obj['Key']:
                         total_size += obj['Size']
-                # total_size += sum(obj['Size'] for obj in page['Contents'] if "temp" in obj['Key'])
-                # for obj in page['Contents']:
-                #     total_size += obj['Size']
+               
         print(f"Total size of all objects in {destination_bucket}. Size: {total_size} bytes")
         
     except Exception as e:
         print(f"Error copied {e}")
         raise e
-    
-    
 
-    # try:
-    #     for record in event['Records']:
-    #         key = record['s3']['object']['key']
-    #         size = record['s3']['object']['size']
-
-    #         # copy object
-    #         copy_source = {'Bucket': source_bucket, 'Key': key}
-    #         s3_client.copy(copy_source, destination_bucket, key)
-    #         print(f"Successfully copied {key} from {source_bucket} to {destination_bucket}. Size: {size} bytes")
-
-    
-
-    #     total_size = 0
-    #     paginator = s3_client.get_paginator('list_objects_v2')
-
-
-    #     for page in paginator.paginate(Bucket=destination_bucket):
-    #         if 'Contents' in page:
-    #             for obj in page['Contents']:
-    #                 total_size += obj['Size']
-    #     print(f"Total size of all objects in {destination_bucket}. Size: {total_size} bytes")
-
-    # except Exception as e:
-    #     print(f"Error copied {e}")
-    #     raise e
          
     # return {
     #     'statusCode': 200,
